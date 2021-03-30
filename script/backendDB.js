183541506cc7c7b5cc01f084f89faa4ef5d92843
HarshG-dev <harshaniegomes@gmail.com>
2021-03-30 20:04:40 +0100

    Added comments to backendDB
    Added comments to backendDB.js

--- script/backendDB.js
+++ script/backendDB.js
@@ -1,3 +1,12 @@
+/**
+* Get data from backend, currently reads a json file directly. 
+* Replace with actual server call 
+* @author  Harshanie Gomes
+* @param   {function} cbOK    callback function , when data is read
+* @param   {function} cbFail  callback function , when data is read fails
+* @return  {}
+*/
+
 function getDataFromDB(cbOK, cbFail) {
     fetch('./db.json')
     .then(resp => resp.json())
