c12cabf5d0abc97e6cc95d143cf2e0b8814c6cba
HarshG-dev <harshaniegomes@gmail.com>
2021-03-30 01:15:58 +0100

    Added Unit Test
    Added Jest testing for slider module.
    Set up Babel as Jest doesn't support ES6 modules.

---
+++ __test__/slider_module.test.js
@@ -0,0 +1,46 @@
+import { expect, test } from "@jest/globals";
+
+document.body.innerHTML = 
+'<main id="main">' +
+'<div id="slider-one">' +
+'<div id="mySlide1" style="display:none;">' + '<p>Card 1</p></div>' +
+'<div id="mySlide1" style="display:none;">' + '<p>Card 2</p></div>' +
+'<div id="mySlide1" style="display:none;">' + '<p>Card 3</p></div>' +
+'<div id="mySlide1" style="display:none;">' + '<p>Card 4</p></div>' +
+'<div id="mySlide1" style="display:none;">' + '<p>Card 5</p></div>' +
+'<div id="mySlide1" style="display:none;">' + '<p>Card 6</p></div>' +
+'<div id="mySlide1" style="display:none;">' + '<p>Card 7</p></div>' +
+'<div id="mySlide1" style="display:none;">' + '<p>Card 8</p></div>' +
+'</div>' +
+'<div>' +
+'   <a class="prev-one">&#10094;</a>' +
+'   <a class="next-one">&#10095;</a>' +
+'</div>' +
+'</main>';
+
+import { Slider } from "../script/slider-module.js";
+
+test("Testing for slider object creation", () => {
+    let mySlider1 = new Slider("#mySlide1",3);
+    expect(mySlider1.slides.length).toBe(8);
+    expect(mySlider1.itemsToShow).toBe(3);
+});
+
+test("Testing for slider functionality", () => {
+    let mySlider1 = new Slider("#mySlide1",3);
+    let prevButton = document.querySelector(".prev-one");
+    let nextButton = document.querySelector(".next-one");
+    mySlider1.registerNav(prevButton,nextButton);
+    expect(mySlider1.currentSlide).toBe(0);
+    mySlider1.init(0);
+    expect(mySlider1.currentSlide).toBe(2);
+    nextButton.click();
+    expect(mySlider1.currentSlide).toBe(5);
+    nextButton.click();
+    expect(mySlider1.currentSlide).toBe(7);
+    // test for nextbutton
+    prevButton.click();
+    expect(mySlider1.currentSlide).toBe(4);
+    prevButton.click();
+    expect(mySlider1.currentSlide).toBe(2);
+});
